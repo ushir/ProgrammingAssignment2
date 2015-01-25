22600274f91a00cae09c3b376681ffc9ea368db4
ushir <ushir@yahoo.com>
2015-01-25 11:42:38 -0800

    Added matrix compare
    Added a new parameter to cacheSolve and compared with old matrix to make
    sure that cell data is not changed which can be done without calling
    makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions creates a matrix, caches the matrix using lexical
+## scoping rules and returns the inverse.  If repeatedly used without
+## changes in original matrix, will not compute the inverse again.
 
-## This function creates a new matrix as specified in argument, stores itin x
-## and stores NULL in obejct m; cachesolve function calls setsolve funxtion to
-## store the inverse of matrix in m
+
+## This function creates a new matrix as specified in argument, stores the  
+## inverse in m when $setsolve is called and provides setsolve and getsolve
+## methods to store and extarct information.
+## 
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL # In theory a check can me made here to see if m is identicle to x
+  m <- NULL
   set <- function(y) {
     x <<- y
     m <<- NULL
@@ -25,16 +28,19 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This functions checks whether m stored in above function is null or not
 ## and based on it eithers retuns cached m or recalculates inverse
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x,newm ...) {
         ## Return a matrix that is the inverse of 'x'
   m <- x$getsolve()
+  oldm <- x$get()
   if(!is.null(m)) {
-    # An additional check can be made here to compare x and m
-    message("getting cached data")
-    return(m)
+    # An additional compare to check the potential changes in matrix data
+    if (identical(oldm,newm)) {
+      message("getting cached data")
+      return(m)
+    }
   }
-  data <- x$get()
-  m <- solve(data,...)  # Inverse of a square matrix
+
+  m <- solve(newm,...)  # Inverse of a square matrix
   x$setsolve(m)
   m
 }
@@ -44,10 +50,16 @@ cacheSolve <- function(x, ...) {
 #> source("cacheSolve.R")
 #> x <- matrix(c(4,2,7,6), nrow=2, ncol=2)
 #> y <- makeCacheMatrix(x)
-#> z <- cacheSolve(y)
+#> z <- cacheSolve(y,x)
 #> z
 #[,1] [,2]
 #[1,]  0.6 -0.7
 #[2,] -0.2  0.4
-#> z <- cacheSolve(y)
+#> z <- cacheSolve(y,x)
 #getting cached data
+#> x[1,1]<-100
+#> z <- cacheSolve(y,x)
+#> z
+#[,1]        [,2]
+#[1,]  0.010238908 -0.01194539
+#[2,] -0.003412969  0.17064846
